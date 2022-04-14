09688bf6291106de7e9f4cdd3ae79461432de0e2
Hayden Smith <hayden@pearler.com>
2022-04-14 22:39:58 +1000

    0.4.2

--- src/RevealNext.js
+++ src/RevealNext.js
@@ -17,32 +17,34 @@ import 'reveal.js/plugin/highlight/monokai.css';
 export default (function (type) {
     var rootId = (type === 'ReactJS' ? '#root' : '#__next');
     return function (_a) {
-        var children = _a.children, options = _a.options;
-        var _b = React.useState(false), loaded = _b[0], setLoaded = _b[1];
+        var children = _a.children, options = _a.options, _b = _a.load, load = _b === void 0 ? true : _b;
+        var _c = React.useState(false), loaded = _c[0], setLoaded = _c[1];
         React.useEffect(function () {
-            Promise.all([
-                import('reveal.js'),
-                import('reveal.js/plugin/markdown/markdown.esm.js'),
-                import('reveal.js/plugin/highlight/highlight'),
-                import('reveal.js/plugin/notes/notes'),
-            ]).then(function (values) {
-                var Reveal = values[0], Markdown = values[1], Highlight = values[2], Notes = values[3];
-                var rootProp = document.querySelector(rootId);
-                if (rootProp) {
-                    rootProp.className = "reveal";
-                }
-                var deck = new Reveal.default(__assign({ plugins: [Markdown.default, Notes.default, Highlight.default] }, options));
-                deck.initialize().then(function () {
-                    deck.on('slidechanged', function (_a) {
-                        var indexh = _a.indexh, indexv = _a.indexv, currentSlide = _a.currentSlide;
-                        var urlstr = "#/".concat(indexh).concat(indexv !== 0 ? '/' + indexv : '');
-                        var newpath = window.location.pathname + urlstr;
-                        window.history.replaceState(null, document.title, newpath);
+            if (load) {
+                Promise.all([
+                    import('reveal.js'),
+                    import('reveal.js/plugin/markdown/markdown.esm.js'),
+                    import('reveal.js/plugin/highlight/highlight'),
+                    import('reveal.js/plugin/notes/notes'),
+                ]).then(function (values) {
+                    var Reveal = values[0], Markdown = values[1], Highlight = values[2], Notes = values[3];
+                    var rootProp = document.querySelector(rootId);
+                    if (rootProp) {
+                        rootProp.className = "reveal";
+                    }
+                    var deck = new Reveal.default(__assign({ plugins: [Markdown.default, Notes.default, Highlight.default] }, options));
+                    deck.initialize().then(function () {
+                        deck.on('slidechanged', function (_a) {
+                            var indexh = _a.indexh, indexv = _a.indexv, currentSlide = _a.currentSlide;
+                            var urlstr = "#/".concat(indexh).concat(indexv !== 0 ? '/' + indexv : '');
+                            var newpath = window.location.pathname + urlstr;
+                            window.history.replaceState(null, document.title, newpath);
+                        });
                     });
+                    setLoaded(true);
                 });
-                setLoaded(true);
-            });
-        }, []);
+            }
+        }, [load]);
         return React.createElement("div", { className: "slides" }, loaded && children);
     };
 });
--- src/RevealNext.tsx
+++ src/RevealNext.tsx
@@ -9,34 +9,36 @@ type FrameworkType = 'ReactJS' | 'NextJS'
 
 export default (type: FrameworkType) => {
   const rootId = (type === 'ReactJS' ? '#root' : '#__next');
-  return ({ children, options }: { children: React.FC, options: any }) => {
+  return ({ children, options, load = true }: { children: React.FC, options: any, load: boolean }) => {
     const [loaded, setLoaded] = React.useState(false);
     React.useEffect(() => {
-      Promise.all([
-        import('reveal.js'),
-        import('reveal.js/plugin/markdown/markdown.esm.js'),
-        import('reveal.js/plugin/highlight/highlight'),
-        import('reveal.js/plugin/notes/notes'),
-      ]).then(values => {
-        const [Reveal, Markdown, Highlight, Notes] = values;
-        const rootProp = document.querySelector(rootId);
-        if (rootProp) {
-          rootProp.className = "reveal";
-        }
-        let deck = new Reveal.default({
-           plugins: [ Markdown.default, Notes.default, Highlight.default ],
-           ...options,
-        })
-        deck.initialize().then(() => {
-          deck.on('slidechanged', ({ indexh, indexv, currentSlide }: SlideChangeProps) => {
-            const urlstr = `#/${indexh}${indexv !== 0 ? '/' + indexv : ''}`
-            const newpath = window.location.pathname + urlstr;
-            window.history.replaceState(null, document.title, newpath);
+      if (load) {
+        Promise.all([
+          import('reveal.js'),
+          import('reveal.js/plugin/markdown/markdown.esm.js'),
+          import('reveal.js/plugin/highlight/highlight'),
+          import('reveal.js/plugin/notes/notes'),
+        ]).then(values => {
+          const [Reveal, Markdown, Highlight, Notes] = values;
+          const rootProp = document.querySelector(rootId);
+          if (rootProp) {
+            rootProp.className = "reveal";
+          }
+          let deck = new Reveal.default({
+             plugins: [ Markdown.default, Notes.default, Highlight.default ],
+             ...options,
+          })
+          deck.initialize().then(() => {
+            deck.on('slidechanged', ({ indexh, indexv, currentSlide }: SlideChangeProps) => {
+              const urlstr = `#/${indexh}${indexv !== 0 ? '/' + indexv : ''}`
+              const newpath = window.location.pathname + urlstr;
+              window.history.replaceState(null, document.title, newpath);
+            });
           });
+          setLoaded(true);
         });
-        setLoaded(true);
-      });
-    }, []);
+      }
+    }, [load]);
     return <div className="slides">{loaded && children}</div>;
   }  
 }
